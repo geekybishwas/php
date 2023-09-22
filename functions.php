320b28a7c9db6e77a7f1c499bd439f88fef17177
geekybishwas <techiebishwas@gmail.com>
2023-09-22 23:04:57 +0545

    funtions

--- functions.php
+++ functions.php
@@ -13,3 +13,19 @@ function show($name)
     echo "Hello" .$name;
 }
 show("geeky");
+
+
+//Funtion with optional arguments
+function defaultArg($name='bob')
+{
+    echo "Hello" .$name;
+}
+defaultArg("geeky"); //Value given at function call overrides the optional arguments
+
+//Funtion with return
+function returnValue($name)
+{
+    return "Hello" .$name
+}
+$value=returnValue("geeky");
+echo $value;
